8dfaed29c27c358b5c1cde8e4acd914f557fd627
merchi92 <webma1132192@gmail.com>
2022-04-12 23:57:59 +0900

    10814, 10989 solved

--- test/solved_ac/P10814.java
+++ test/solved_ac/P10814.java
@@ -1,12 +1,13 @@
 package solved_ac;
 
 import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 // 나이순 정렬
 // https://www.acmicpc.net/problem/10814
+// 22.04.12 못풀었음 (시간초과)
 
 public class P10814 {
 	public static void solution() {
@@ -21,15 +22,54 @@ public class P10814 {
 			list.add(new String[] {String.valueOf(age), name});
 		}
 		
+		selectionSort(n, list);
 		
+		for(int i=0;i<n;i++) {
+			System.out.println(list.get(i)[0] + " " + list.get(i)[1]);
+		}
 	}
 	
 	public static void selectionSort(int n, ArrayList<String[]> list) {
-		for(int i=0;i<n;i++) {
-			for(int j=i+1;j<n-1;j++) {
-				
-				
+		for(int i=0;i<n-1;i++) {
+			int j = i + 1;
+			while(j > 0) {
+				if(Integer.parseInt(list.get(j)[0]) < Integer.parseInt(list.get(j-1)[0])) {
+					swap(list, j);
+				}
+				j--;
 			}
+		}
+	}
+	
+	public static void swap(ArrayList<String[]> list, int j) {
+		String[] temp = new String[2];
+		temp = list.get(j-1);
+		list.set(j-1, list.get(j));
+		list.set(j, temp);
+	}
+
+	// Arrays.sort(Array, Comparator) 재정의하여 구현
+	// https://st-lab.tistory.com/243
+	public static void solution2() {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[][] list = new String[n][2];
+		
+		for(int i=0;i<n;i++) {
+			list[i][0] = sc.next();
+			list[i][1] = sc.next();
+		}
+		
+		Arrays.sort(list, new Comparator<String[]>() {
+			// 나이순으로 정렬
+			@Override
+			public int compare(String[] s1, String[] s2) {
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+			}
+		});
+		
+		for(int i=0;i<n;i++) {
+			System.out.println(list[i][0] + " " + list[i][1]);
 		}
 	}
 }
--- test/solved_ac/P10989.java
+++ test/solved_ac/P10989.java
@@ -1,19 +1,40 @@
 package solved_ac;
 
+// 수 정렬하기 3
+// https://www.acmicpc.net/problem/10989
+// 22.04.12 못풀었음(시간초과, 데이터초과)
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		solution();
+public class P10989 {
+	
+	// https://st-lab.tistory.com/107 참조
+	// 방법 1. Arrays.sort() 사용하기
+	public static void solution() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i=0;i<n;i++) {
+			sb.append(arr[i]).append('\n');
+		}
+		
+		System.out.println(sb);
 	}
 	
-	public static void solution() throws IOException{
+	// 방법 2. 카운팅 정렬
+	public static void solution2() throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		// 수의 범위 ( 0 ~ 10000), 문제 조건상 0은 제외
