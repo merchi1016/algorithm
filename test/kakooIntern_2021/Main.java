dd502a9f5fb8b7f62f3ba9305fe573951126f4e0
merchi92 <webma1132192@gmail.com>
2022-04-14 23:51:37 +0900

    2021 kakao 인턴쉽 문제-숫자 문자열과 영단어 풀기

---
+++ test/kakooIntern_2021/Main.java
@@ -0,0 +1,9 @@
+package kakooIntern_2021;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ test/kakooIntern_2021/StringNumber.java
@@ -0,0 +1,25 @@
+package kakooIntern_2021;
+
+// 숫자 문자열과 영단어
+// https://programmers.co.kr/learn/courses/30/lessons/81301
+
+public class StringNumber {
+	public static int solution(String s) {
+		int answer = 0;
+		
+		s = s.replaceAll("zero", "0");
+        s = s.replaceAll("one", "1");
+        s = s.replaceAll("two", "2");
+        s = s.replaceAll("three", "3");
+        s = s.replaceAll("four", "4");
+        s = s.replaceAll("five", "5");
+        s = s.replaceAll("six", "6");
+        s = s.replaceAll("seven", "7");
+        s = s.replaceAll("eight", "8");
+        s = s.replaceAll("nine", "9");
+
+        answer = Integer.parseInt(s);
+
+		return answer;
+	}
+}
